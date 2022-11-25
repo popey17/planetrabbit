c678232a20d28b9c6747cb76d81cdf14fcf0e042
P0p3yG0d <popeyleo25@gmail.com>
2022-11-25 13:36:18 +0630

    first commit

--- js/activepage.js
+++ js/activepage.js
@@ -3,5 +3,7 @@ const navLink = document.querySelectorAll('#navbarNav ul li a').
 forEach(link=> {
     if(link.href.includes(`${activePage}`)){
         link.classList.add('active');
+    }else {
+        link.classList.remove('active');
     }
 })
